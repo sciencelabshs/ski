2c9ee9575577211a6063019215863554abd00119
sciencelabshs <46806543+sciencelabshs@users.noreply.github.com>
2024-03-23 21:01:11 +0530

    Update ice.js

--- js/ice.js
+++ js/ice.js
@@ -687,7 +687,7 @@ let head_section;
             }
         }
 
-        download_file("/models/ice_vertices.dat", function(buffer) {
+        download_file("/ski/models/ice_vertices.dat", function(buffer) {
             gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
             gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(buffer), gl.STATIC_DRAW);
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -695,7 +695,7 @@ let head_section;
             mark_ready();
         });
 
-        download_file("/models/ice_indices.dat", function(buffer) {
+        download_file("/ski/models/ice_indices.dat", function(buffer) {
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(buffer), gl.STATIC_DRAW);
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
